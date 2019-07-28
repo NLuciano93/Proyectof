1bcf3f640b98c9fb1631fad853ad7742b76cac37
NLuciano93 <natielloluciano@gmail.com>
2019-07-28 13:09:34 -0300

    Esquema registro

--- vistas/registro/index.php
+++ vistas/registro/index.php
@@ -22,13 +22,14 @@
 				<nav>
 
 				  <div class="nav nav-tabs" id="nav-tab" role="tablist">
-				    <a class="nav-item nav-link active " id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Registro Usuario</a>
-				    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Registro Profesor/a</a>
+				    <a class="nav-item nav-link active " id="nav-usuario-tab" data-toggle="tab" href="#nav-usuario" role="tab" aria-controls="nav-usuario" aria-selected="true">Registro Usuario</a>
+				    <a class="nav-item nav-link" id="nav-profe-tab" data-toggle="tab" href="#nav-profe" role="tab" aria-controls="nav-profe" aria-selected="false">Registro Profesor/a</a>
+				     <a class="nav-item nav-link" id="nav-gimnasio-tab" data-toggle="tab" href="#nav-gimnasio" role="tab" aria-controls="nav-gimnasio" aria-selected="false">Registro Gimnasio</a>
 				    
 				  </div>
 				</nav>
 				<div class="tab-content" id="nav-tabContent">
-				  <div class="tab-pane fade show active shadow-lg p-3 mb-5 bg-white rounded" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+				  <div class="tab-pane fade show active shadow-lg p-3 mb-5 bg-white rounded" id="nav-usuario" role="tabpanel" aria-labelledby="nav-usuario-tab">
 				  	<form class="form-horizontal border ">
 					<fieldset>
 
@@ -37,18 +38,9 @@
 
 					<!-- Text input-->
 					<div class="form-group">
-					  <label class="col-md-4 control-label" for="Nombre">Nombre</label>  
+					  <label class="col-md-4 control-label" for="Nombre">Nombre completo</label>  
 					  <div class="col-md-4">
 					  <input id="Nombre" name="Nombre" type="text" placeholder="" class="form-control input-md" required="">
-					    
-					  </div>
-					</div>
-
-					<!-- Text input-->
-					<div class="form-group">
-					  <label class="col-md-4 control-label" for="Apellido">Apellido</label>  
-					  <div class="col-md-4">
-					  <input id="Apellido" name="Apellido" type="text" placeholder="" class="form-control input-md" required="">
 					    
 					  </div>
 					</div>
@@ -67,6 +59,15 @@
 					  <label class="col-md-4 control-label" for="mail">Mail</label>  
 					  <div class="col-md-4">
 					  <input id="mail" name="mail" type="text" placeholder="" class="form-control input-md" required="">
+					    
+					  </div>
+					</div>
+
+					<!-- Text input-->
+					<div class="form-group">
+					  <label class="col-md-4 control-label" for="edad">Edad</label>  
+					  <div class="col-md-4">
+					  <input id="edad" name="edad" type="text" placeholder="" class="form-control input-md">
 					    
 					  </div>
 					</div>
@@ -111,9 +112,18 @@
 					  </div>
 					</div>
 
+					<!-- foto -->
+
+					<div class="form-group">
+                            <label for="exampleFormControlFile1">Agrega foto perfil</label>
+                             <input type="file" class="form-control-file" id="exampleFormControlFile1" name="foto" required>
+                      </div>							
+
+
+
 					<!-- Text input-->
 					<div class="form-group">
-					  <label class="col-md-4 control-label" for="facebook">Perfil de Facebook</label>  
+					  <label class="col-md-4 control-label" for="facebook">Perfil de Facebook (ejemplo: https://www.facebook.com/...)</label>  
 					  <div class="col-md-4">
 					  <input id="facebook" name="facebook" type="text" placeholder="" class="form-control input-md">
 					    
@@ -146,7 +156,7 @@
 
 
 				  </div>
-				  <div class="tab-pane fade shadow-lg p-3 mb-5 bg-white rounded" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+				  <div class="tab-pane fade shadow-lg p-3 mb-5 bg-white rounded" id="nav-profe" role="tabpanel" aria-labelledby="nav-profe-tab">
 
 
 				  	<form class="form-horizontal">
@@ -157,18 +167,198 @@
 
 										<!-- Text input-->
 										<div class="form-group">
-										  <label class="col-md-4 control-label" for="Nombre">Nombre</label>  
+										  <label class="col-md-4 control-label" for="Nombre">Nombre completo</label>  
 										  <div class="col-md-4">
 										  <input id="Nombre" name="Nombre" type="text" placeholder="" class="form-control input-md" required="">
+										    
+										  </div>
+										</div>
+					
+
+										<!-- Text input-->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="telefono">Teléfono/celular</label>  
+										  <div class="col-md-4">
+										  <input id="telefono" name="telefono" type="text" placeholder="" class="form-control input-md">
+										    
+										  </div>
+										</div>
+
+									
+
+										<!-- Text input-->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="mail">Mail</label>  
+										  <div class="col-md-4">
+										  <input id="mail" name="mail" type="text" placeholder="" class="form-control input-md" required="">
+										    
+										  </div>
+										</div>
+								<!-- Text input-->
+											<div class="form-group">
+											  <label class="col-md-4 control-label" for="edad">Edad</label>  
+											  <div class="col-md-4">
+											  <input id="edad" name="edad" type="text" placeholder="" class="form-control input-md">
+											    
+											  </div>
+											</div>	
+
+										<!-- Select Basic -->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="provincia">Provincia</label>
+										  <div class="col-md-4">
+										    <select id="provincia" name="provincia" class="form-control">
+										      <option value="1">Buenos Aires</option>
+										      <option value="2">Caba</option>
+										    </select>
+										  </div>
+										</div>
+
+										<!-- Select Basic -->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="localidad">Localidad</label>
+										  <div class="col-md-4">
+										    <select id="localidad" name="localidad" class="form-control">
+										      <option value="1">San Isidro</option>
+										      <option value="2">San fernando</option>
+										    </select>
+										  </div>
+										</div>
+
+										<!-- Password input-->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="contrasena">Contraseña</label>
+										  <div class="col-md-4">
+										    <input id="contrasena" name="contrasena" type="password" placeholder="" class="form-control input-md" required="">
+										    
+										  </div>
+										</div>
+
+										<!-- Password input-->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="repcontrasena">Repetir Contraseña</label>
+										  <div class="col-md-4">
+										    <input id="repcontrasena" name="repcontrasena" type="password" placeholder="" class="form-control input-md" required="">
 										    
 										  </div>
 										</div>
 
 										<!-- Text input-->
 										<div class="form-group">
-										  <label class="col-md-4 control-label" for="Apellido">Apellido</label>  
+										  <label class="col-md-4 control-label" for="facebook">Perfil de Facebook (ejemplo: https://www.facebook.com/...)</label>  
 										  <div class="col-md-4">
-										  <input id="Apellido" name="Apellido" type="text" placeholder="" class="form-control input-md" required="">
+										  <input id="facebook" name="facebook" type="text" placeholder="" class="form-control input-md">
+										    
+										  </div>
+										</div>
+
+										<!-- Text input-->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="twitter">Twitter</label>  
+										  <div class="col-md-4">
+										  <input id="twitter" name="twitter" type="text" placeholder="" class="form-control input-md">
+										    
+										  </div>
+										</div>
+
+										<!-- Text input-->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="instagram">Instagram</label>  
+										  <div class="col-md-4">
+										  <input id="instagram" name="instagram" type="text" placeholder="" class="form-control input-md">
+										    
+										  </div>
+										</div>
+
+										<!-- Textarea -->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="descripcion">Descripción</label>
+										  <div class="col-md-4">                     
+										    <textarea class="form-control" id="descripcion" name="descripcion">Comentario acerca de ti</textarea>
+										  </div>
+										</div>
+
+										<!-- foto -->
+
+										<div class="form-group">
+					                            <label for="exampleFormControlFile1">Agrega foto perfil</label>
+					                             <input type="file" class="form-control-file" id="exampleFormControlFile1" name="foto" required>
+					                      </div>	
+
+										<!-- File Button --> 
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="cv">Subir CV</label>
+										  <div class="col-md-4">
+										    <input id="cv" name="cv" class="input-file" type="file">
+										  </div>
+										</div>
+
+										<!-- Multiple Checkboxes -->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="especialidad">Especialidades</label>
+										  <div class="col-md-4">
+										  <div class="checkbox">
+										    <label for="especialidad-0">
+										      <input type="checkbox" name="especialidad" id="especialidad-0" value="1">
+										      Personal Trainer
+										    </label>
+											</div>
+										  <div class="checkbox">
+										    <label for="especialidad-1">
+										      <input type="checkbox" name="especialidad" id="especialidad-1" value="2">
+										      Deportes
+										    </label>
+											</div>
+										  <div class="checkbox">
+										    <label for="especialidad-2">
+										      <input type="checkbox" name="especialidad" id="especialidad-2" value="3">
+										      Funcional
+										    </label>
+											</div>
+										  <div class="checkbox">
+										    <label for="especialidad-3">
+										      <input type="checkbox" name="especialidad" id="especialidad-3" value="4">
+										      Crossfit 
+										    </label>
+											</div>
+										  <div class="checkbox">
+										    <label for="especialidad-4">
+										      <input type="checkbox" name="especialidad" id="especialidad-4" value="5">
+										     Yoga
+										    </label>
+											</div>
+										  </div>
+										</div>
+
+										<!-- Button -->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for=""></label>
+										  <div class="col-md-4">
+										    <button id="" name="" class="btn btn-primary">Registrar</button>
+										  </div>
+										</div>
+
+										</fieldset>
+										</form>
+
+				  	
+				 				 </div>
+				  
+
+				  <div class="tab-pane fade shadow-lg p-3 mb-5 bg-white rounded" id="nav-gimnasio" role="tabpanel" aria-labelledby="nav-gimnasio-tab">
+
+
+				  	<form class="form-horizontal">
+										<fieldset>
+
+										<!-- Form Name -->
+										<legend class="d-flex justify-content-center">Registro Gimnasio</legend>
+
+										<!-- Text input-->
+										<div class="form-group">
+										  <label class="col-md-4 control-label" for="Nombre">Nombre completo</label>  
+										  <div class="col-md-4">
+										  <input id="Nombre" name="Nombre" type="text" placeholder="" class="form-control input-md" required="">
 										    
 										  </div>
 										</div>
@@ -233,7 +423,7 @@
 
 										<!-- Text input-->
 										<div class="form-group">
-										  <label class="col-md-4 control-label" for="facebook">Perfil de Facebook</label>  
+										  <label class="col-md-4 control-label" for="facebook">Perfil de Facebook (ejemplo: https://www.facebook.com/...)</label>  
 										  <div class="col-md-4">
 										  <input id="facebook" name="facebook" type="text" placeholder="" class="form-control input-md">
 										    
@@ -262,38 +452,46 @@
 										<div class="form-group">
 										  <label class="col-md-4 control-label" for="descripcion">Descripción</label>
 										  <div class="col-md-4">                     
-										    <textarea class="form-control" id="descripcion" name="descripcion">Comentario acerca de ti</textarea>
+										    <textarea class="form-control" id="descripcion" name="descripcion">Comentario acerca del local</textarea>
 										  </div>
 										</div>
 
-										<!-- File Button --> 
+										<!-- Textarea -->
 										<div class="form-group">
-										  <label class="col-md-4 control-label" for="cv">Subir CV</label>
-										  <div class="col-md-4">
-										    <input id="cv" name="cv" class="input-file" type="file">
+										  <label class="col-md-4 control-label" for="Horarios">Horarios</label>
+										  <div class="col-md-4">                     
+										    <textarea class="form-control" id="Horarios" name="Horarios">(EJ: Lu a Vi 8 a 22hs)</textarea>
 										  </div>
 										</div>
+
+										<!-- foto -->
+
+										<div class="form-group">
+					                            <label for="exampleFormControlFile1">Agrega foto perfil</label>
+					                             <input type="file" class="form-control-file" id="exampleFormControlFile1" name="foto" required>
+					                      </div>	
+
 
 										<!-- Multiple Checkboxes -->
 										<div class="form-group">
-										  <label class="col-md-4 control-label" for="especialidad">Especialidades</label>
+										  <label class="col-md-4 control-label" for="especialidad">Actividades</label>
 										  <div class="col-md-4">
 										  <div class="checkbox">
 										    <label for="especialidad-0">
 										      <input type="checkbox" name="especialidad" id="especialidad-0" value="1">
-										      Personal Trainer
-										    </label>
+										      Musculación 										    
+										  </label>
 											</div>
 										  <div class="checkbox">
 										    <label for="especialidad-1">
 										      <input type="checkbox" name="especialidad" id="especialidad-1" value="2">
-										      Funcional
+										     Crossfit
 										    </label>
 											</div>
 										  <div class="checkbox">
 										    <label for="especialidad-2">
 										      <input type="checkbox" name="especialidad" id="especialidad-2" value="3">
-										      Crossfit
+										      Funcional
 										    </label>
 											</div>
 										  <div class="checkbox">
@@ -305,7 +503,7 @@
 										  <div class="checkbox">
 										    <label for="especialidad-4">
 										      <input type="checkbox" name="especialidad" id="especialidad-4" value="5">
-										      Deportes
+										      Zumba
 										    </label>
 											</div>
 										  </div>
@@ -324,7 +522,20 @@
 
 				  	
 				 				 </div>
-				  
+
+							
+
+
+
+
+
+
+
+
+
+
+
+
 				</div>
 			</div>
 		</div>
